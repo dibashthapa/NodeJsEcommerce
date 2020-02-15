744b774b3e13899b0520aa368e7f6538631ff2d5
dibashthapa <dibashthapa55@gmail.com>
2020-02-15 13:35:18 +0545

    initial upload

---
+++ app/controllers/search.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+router.get('/search', (req, res, next) => {
+name= req.query.name;
+  axios.get("https://ecommeceapi.herokuapp.com/search?name="+name)
+  .then((api_data)=>{
+    response= JSON.parse(JSON.stringify(api_data.data));
+res.render("items",{data:response})
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+
+});
+
+module.exports = router;
