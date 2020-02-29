9224bb49eabc320b32f61927dfd39cd30c32fc74
dibashthapa <dibashthapa55@gmail.com>
2020-02-29 15:19:57 +0545

    initial upload

--- app/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports= function(app){
-  app.use('/',require('./controllers/home'))
-  app.use('/',require('./controllers/categories'))
-  app.use('/',require('./controllers/search'))
-  app.use('/',require('./controllers/cart'))
-}
